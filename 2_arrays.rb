d6164e0b52b905e22309674abe0521261b332abf
monicamow <mow.monica@gmail.com>
2016-05-28 21:53:24 -0700

    first commit

---
+++ 2_arrays.rb
@@ -0,0 +1,57 @@
+### SETUP
+require 'rspec'
+
+### LOGIC (fix me)
+
+# Returns the average of all the numbers in the array
+def average(numbers)
+  return nil if numbers.nil?
+  return nil if numbers.empty?
+
+  if numbers.any? { |element| element.class == String }
+    average(no_strings(numbers))
+  else
+    sum = 0
+    numbers.each do |n|
+      sum += n
+    end
+    sum / numbers.size
+  end
+end
+
+def no_strings(numbers)
+  numbers_without_strings = []
+  numbers.map do |str| 
+    if str.is_a? String
+      numbers_without_strings << str.to_i
+    else
+      numbers_without_strings << str
+    end
+  end
+  numbers = numbers_without_strings
+end
+
+### TEST CODE (don't touch me)
+
+describe '#average' do
+  it 'returns nil for empty array' do
+    result = average([])
+    expect(result).to be_nil
+  end
+  it 'returns nil when nil is passed in' do
+    result = average(nil)
+    expect(result).to be_nil
+  end
+  it 'returns 4 for 3,4,5' do
+    result = average([3, 4, 5])
+    expect(result).to eq(4)
+  end
+  it 'can handle numbers represented as strings' do
+    result = average([10, '20', 30])
+    expect(result).to eq(20)
+  end
+  it 'can handle floats' do
+    result = average([1.0, 1.5, 2.0])
+    expect(result).to eq(1.5)
+  end
+end
